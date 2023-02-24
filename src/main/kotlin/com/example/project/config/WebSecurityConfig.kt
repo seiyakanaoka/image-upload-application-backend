3a35c5102c55ec2af1a22117255f9357610a6c2a
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-02-24 17:22:02 +0900

    feat: spring security実装

---
+++ src/main/kotlin/com/example/project/config/WebSecurityConfig.kt
@@ -0,0 +1,47 @@
+package com.example.project.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+    @Bean
+    fun configureHttpSecurity(httpSecurity: HttpSecurity): SecurityFilterChain {
+        httpSecurity
+            .authorizeHttpRequests()
+            .requestMatchers("/", "/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+            .formLogin()
+            .loginProcessingUrl("/login")
+            .usernameParameter("email")
+            .passwordParameter("password")
+        return httpSecurity.build()
+    }
+
+    @Bean
+    fun users(): UserDetailsService {
+        val user = User.builder()
+            .username("user")
+            .password(passwordEncoder()?.encode("password"))
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder? {
+        return BCryptPasswordEncoder()
+    }
+}
--- src/main/kotlin/com/example/project/user/controller/UserController.kt
+++ src/main/kotlin/com/example/project/user/controller/UserController.kt
@@ -2,8 +2,9 @@ package com.example.project.user.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class UserController {
     @GetMapping("/")
     fun index(): String {
