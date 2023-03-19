764b89d6d6e71bf7096dc8de35bdfc9cb138f7d9
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-03-19 12:13:31 +0900

    feat: configファイルにS3の設定を追加

---
+++ src/main/kotlin/com/example/project/config/aws/S3Config.kt
@@ -0,0 +1,31 @@
+package com.example.project.config.aws
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class S3Config {
+    /**
+     * ローカルからクレデンシャル情報を取得してS3を操作する設定
+     * ConfigにS3クライアントを設定することで、DIで使用することができる
+     * */
+    @Bean
+    fun s3client(): AmazonS3 {
+        return AmazonS3ClientBuilder.defaultClient()
+    }
+}
+
+/**
+ * 以下のように、クレデンシャルを明示的に指定する方法もある
+ */
+//@Bean
+//fun s3client(): AmazonS3 {
+//    val credentials = BasicAWSCredentials(accessKey, secretKey)
+//
+//    return AmazonS3ClientBuilder.standard()
+//        .withRegion(Regions.fromName(region))
+//        .withCredentials(AWSStaticCredentialsProvider(credentials))
+//        .build()
+//}
